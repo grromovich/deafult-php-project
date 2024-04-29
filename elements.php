d3f814169026917a781a3adec91741d8a02c8bf2
Гусь молодец <Legendaryperson@yandex.ru>
2024-04-29 00:01:13 +0000

    Initial commit from PHPSandbox

---
+++ elements.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900" rel="stylesheet">
+    
+        <link rel="stylesheet" type="text/css" href="base.css">
+    </head>
+    <body>
+        <div class="container">
+            <input class="input"/> 
+            <h1>Это h1</h1>
+            <p>Я просто текст</p>
+            <p class="error">Это ошибка детка</p>
+            <button class="btn">Отправить</button>
+            <br />
+            <a href="/">Назад</a>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900" rel="stylesheet">
+        
+        <link rel="stylesheet" type="text/css" href="base.css">
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <script src="index.js" defer></script>
+    </head>
+    <body>
+        <div class="container">
+            <a href="/elements.php">Просто добавь воды</a>
+            <br />
+            <?php
+                echo "Я больной кодер пишу код по 10 часов"
+            ?>  
+        </div>
+    </body>
+</html>
